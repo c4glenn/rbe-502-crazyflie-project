9647a7d565c05432cb285ee70f580cf3cd2c20c4
Philip Smith <philip@randomsmiths.com>
2025-11-21 20:14:34 -0500

    rewrite circle to fit new requirements

--- gym_pybullet_drones/trajectory/circle.py
+++ gym_pybullet_drones/trajectory/circle.py
@@ -1,63 +1,39 @@
 import numpy as np
 
+from polynomial_traj import Trajectory
+
 
 RADIUS = 1
 HOVER_HEIGHT = 1
-OMEGA = 1
-START_POS = np.array([0, 0, 0.5])
-START_VEL = np.array([0, 0, 0])
-START_ACC = np.array([0, 0, 0])
+V_MAX=2.5
+START = np.array([[0, 0, 0.5], [0, 0, 0], [0, 0, 0]])
+CIRCLE_POINT = np.array([[1, 0, 1], [0, 0, 0], [0, 0, 0]])
 
+start_to_circ = Trajectory(3).add_point(0.0, START).add_point(5.0, CIRCLE_POINT).build()
+circ_to_start = Trajectory(3).add_point(10.0, CIRCLE_POINT).add_point(15.0, START).build()
+theta_traj = Trajectory(3).add_point(0.0, np.array([[0.0], [0.0], [0.0]])).add_point(5.0, np.array([[2 * np.pi], [0.0], [0.0]])).build()
+
+# for arr in omega_traj.traj.values():
+#     for i in arr:
+#         print(i[0])
+
+def get_omega(t):
+    return theta_traj.get_values(t)[0][0] * 1/t
+def get_omega_dot(t):
+    return theta_traj.get_values(t)[1][0]
+def get_omega_ddot(t):
+    return theta_traj.get_values(t)[2][0]
 
 def get_circle_pos(t: float) -> np.ndarray:
-    return np.array([RADIUS*np.cos(OMEGA*t), RADIUS*np.sin(OMEGA*t), HOVER_HEIGHT])
+    return np.array([RADIUS*np.cos(get_omega(t)*t), RADIUS*np.sin(get_omega(t)*t), HOVER_HEIGHT])
+
 def get_circle_vel(t: float) -> np.ndarray:
-    return np.array([-RADIUS*OMEGA*np.sin(OMEGA*t), RADIUS*OMEGA*np.cos(OMEGA*t), 0])
+    return np.array([-RADIUS*get_omega_dot(t)*np.sin(get_omega(t)*t),RADIUS*get_omega_dot(t)*np.cos(get_omega(t)*t),0])
+
 def get_circle_acc(t:float) -> np.ndarray:
-    return np.array([-RADIUS*OMEGA*OMEGA*np.cos(OMEGA*t), -RADIUS*OMEGA*OMEGA*np.sin(OMEGA*t), 0])
-
-def get_polynomial_pos(t:float, a:np.ndarray) -> np.ndarray:
-    pos = a[0]
-    for i, a_comp in enumerate(a[1:], 1):
-        pos += a_comp*t**i
-    return pos
-def get_polynomial_vel(t:float, a:np.ndarray) -> np.ndarray:
-    vel = a[1]
-    for i, a_comp in enumerate(a[2:], 2):
-        vel += i*a_comp*t**(i-1)
-    return vel
-def get_polynomial_acc(t:float, a:np.ndarray) -> np.ndarray:
-    acc = 2 * a[2]
-    for i, a_comp in enumerate(a[3:], 3):
-        acc += i*(i-1)*a_comp*t**(i-2)
-    return acc
+    return np.array([-RADIUS*(get_omega_ddot(t)*np.sin(get_omega(t)*t) + (get_omega_dot(t) ** 2) * np.cos(get_omega(t)*t)),RADIUS*(get_omega_ddot(t)*np.cos(get_omega(t)*t) - (get_omega_dot(t) ** 2)*np.sin(get_omega(t)*t)),0])
 
 
-def M_matrix(t: float) -> np.ndarray:
-    return np.array([[1, t, t**2, t**3, t**4, t**5], 
-                     [0, 1, 2*t, 3*t**2, 4*t**3, 5*t**4], 
-                     [0, 0, 2, 6*t, 12*t**2, 20*t**3]])
-
-def create_polynomial_trajectory(start_pos: np.ndarray, start_vel: np.ndarray, start_acc: np.ndarray, start_time: float, end_pos: np.ndarray, end_vel: np.ndarray, end_acc: np.ndarray, end_time: float) -> np.ndarray:
-    """Create a polynomial trajectory 
-
-    Args:
-        start_pos (np.ndarray, shape (3,)): starting position [x, y, z]
-        start_vel (np.ndarray, shape (3,)): starting velocity [vx, vy, vz]
-        start_acc (np.ndarray, shape (3,)): starting acceleration [ax, ay, az]
-        start_time (float): time to start from
-        end_pos (np.ndarray, shape (3,)): ending position [x, y, z]
-        end_vel (np.ndarray, shape (3,)): ending velocity [vx, vy, vz]
-        end_acc (np.ndarray, shape (3,)): ending acceleration [ax, ay, az]
-        end_time (float): time to be done by
-    
-    Returns:
-        a_coef (np.ndarray, shape (6, 3)): coeficents for the polonomials for x, y, and z respectively
-    """
-    A = np.vstack([M_matrix(start_time), M_matrix(end_time)])
-    b = np.array([start_pos, start_vel, start_acc, end_pos, end_vel, end_acc])
-    a = np.linalg.solve(A, b)
-    return a
     
 def circle(t, tf=8):
     """
@@ -80,20 +56,20 @@ def circle(t, tf=8):
             - 'yaw'   (float): Desired yaw angle (set to zero).
             - 'yawdot' (float): Desired yaw rate (set to zero).
     """
-    start_to_circ = create_polynomial_trajectory(START_POS, START_VEL, START_ACC, 0.0, get_circle_pos(5.0), get_circle_vel(5.0), get_circle_acc(5.0), 5)
-    circ_to_start = create_polynomial_trajectory(get_circle_pos(10.0), get_circle_vel(10.0), get_circle_acc(10.0), 10.0, START_POS, START_VEL, START_ACC, tf)
     if t <= 5.0:
-        pos = get_polynomial_pos(t, start_to_circ)
-        vel = get_polynomial_vel(t, start_to_circ)
-        acc = get_polynomial_acc(t, start_to_circ)
+        vals = start_to_circ.get_values(t)
+        pos = vals[0]
+        vel = vals[1]
+        acc = vals[2]
     elif t <= 10.0:
-        pos = get_circle_pos(t)
-        vel = get_circle_vel(t)
-        acc = get_circle_acc(t)
+        pos = get_circle_pos(t-5)
+        vel = get_circle_vel(t-5)
+        acc = get_circle_acc(t-5)
     else: # phase 3
-        pos = get_polynomial_pos(t, circ_to_start)
-        vel = get_polynomial_vel(t, circ_to_start)
-        acc = get_polynomial_acc(t, circ_to_start)
+        vals = circ_to_start.get_values(t)
+        pos = vals[0]
+        vel = vals[1]
+        acc = vals[2]
     
     desired_state = {
         'pos': pos,
---
+++ gym_pybullet_drones/trajectory/polynomial_traj.py
@@ -0,0 +1,64 @@
+import numpy as np
+from math import factorial    
+from typing import List, Tuple 
+from itertools import product
+
+def M_matrix(t: float, constraints: int, derivatives: int) -> np.ndarray:
+    M = np.zeros((derivatives, constraints))
+    for i in range(derivatives):
+        for j in range(i, constraints):
+            M[i, j] = factorial(j)/factorial(j-i) * (t ** (j-i))
+    return M
+
+def find_coefs(start_time:float, end_time:float, start_constraints:np.ndarray, end_constraints:np.ndarray, derivatives_to_calculate:int) -> np.ndarray:
+    """_summary_
+
+    Args:
+        start_time (float): time this polynomial starts at
+        end_time (float): time this polynomial ends at
+        start_constraints (np.ndarray, shape (n,m)): n is number of derivatives and m is number of variables 
+        end_constraints (np.ndarray, shape (n,m)): n is number of derivatives and m is number of variables
+
+    Returns:
+        np.ndarray: _description_
+    """
+    B = np.vstack((start_constraints, end_constraints))
+    A = np.vstack([M_matrix(start_time, B.shape[0], derivatives_to_calculate), M_matrix(end_time, B.shape[0], derivatives_to_calculate)])
+    coefs = np.linalg.solve(A, B)
+    return coefs
+
+
+def get_polynomial_result(time:float, coefs:np.ndarray, derivatives:int) -> np.ndarray:
+    num_constraints = coefs.shape[0]
+    M = M_matrix(time, num_constraints, derivatives)
+    P = M@coefs
+    return P
+
+class Trajectory:
+    def __init__(self, num_derivs:int):
+        self.points = {}
+        self.traj = {}
+        self.num_derivs = num_derivs
+    
+    def add_point(self, time:float, constraint: np.ndarray) -> "Trajectory":
+        self.points[time] = constraint
+        return self
+    
+    def build(self, merge:bool=False) -> "Trajectory":
+        sorted_time = sorted(self.points.keys())
+        if not merge:
+            for time_idx, time in enumerate(sorted_time[:-1]):
+                next_time = sorted_time[time_idx + 1]
+                self.traj[(time, next_time)] = find_coefs(time, next_time, self.points[time], self.points[next_time], self.num_derivs)
+        else:
+            B = np.vstack([self.points[t] for t in sorted_time]) 
+            A = np.vstack([M_matrix(t, B.shape[0], self.num_derivs) for t in sorted_time])
+            print(A)
+            self.traj[(sorted_time[0], sorted_time[-1])] = np.linalg.solve(A, B)
+        return self
+    
+    def get_values(self, time:float):
+        for t1, t2 in self.traj.keys():
+            if t1 <= time and time <= t2:
+                
+                return get_polynomial_result(time, self.traj[(t1, t2)], self.num_derivs)
